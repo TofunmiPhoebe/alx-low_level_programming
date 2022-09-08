4935352726908d25d2572d0c527b8589420cf00d
Akindele Oluwatofunmi <tofunmiphoebe@gmail.com>
2022-09-08 13:16:53 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the function
- * Return: Always 0 (Success)
- */
+* main - print the string in the function
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art, \n")
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,
+\n")
+return (0);
 }
