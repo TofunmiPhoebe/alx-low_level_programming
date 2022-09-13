a6bf1060a8f0f2098894ba4f4c87a55f50fa264b
Akindele Oluwatofunmi <tofunmiphoebe@gmail.com>
2022-09-13 08:58:37 -0700

    My commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Nothing
  */
