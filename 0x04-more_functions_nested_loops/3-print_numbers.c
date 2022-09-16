db4a1dc3d0d58a60cab8be4958fe83049a9b84f6
Akindele Oluwatofunmi <tofunmiphoebe@gmail.com>
2022-09-15 18:20:16 -0700

    My commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,8 @@
  * print_numbers - a function that prints the numbers, from 0 to 9
  * Return: 0-9 followed by newline
  */
-void print_numbers(void)
+int main(void)
 {
-	int x;
-
-	for (x = 0; x <= 9; x++)
-	{
-		_putchar(x + '0');
-	}
-
-	_putchar('\n');
+	print_numbers();
+	return (0);
 }
