9e29b4753871970853a2424b4cf77aacc4a18497
Akindele Oluwatofunmi <tofunmiphoebe@gmail.com>
2022-09-21 12:22:07 -0700

    Commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,17 @@
 char *leet(char *s)
 {
 	int i = 0, j;
-	char substit[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	char subst[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
 	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
-			if (s[i] == str[j]
-					s[i] = substit[j];
+			if (s[i] == str[j])
+				s[i] = subst[j];
 
-					i++;
-					}
-					return (s);
-					}
+		i++;
+	}
+
+	return (s);
+}
