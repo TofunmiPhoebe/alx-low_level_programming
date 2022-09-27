ee34cbfc3f90cfa0aaff777c2badc7ab146e691e
Akindele Oluwatofunmi <tofunmiphoebe@gmail.com>
2022-09-27 09:38:09 -0700

    Commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to cha
+ * @s: the value to modify
+ * @to: the value to assignReturn: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
